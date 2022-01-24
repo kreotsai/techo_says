91499b5956b3cfaeb8761c6846888b052a89173f
alexa <kreotsai@gmail.com>
2022-01-23 21:33:06 -0500

    adding sec key

--- src/techo_says/settings.py
+++ src/techo_says/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
+
+
+SECRET_KEY = config('SECRET_KEY')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
